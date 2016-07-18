ab1f9390bfee071d325d9b9c23923a3569d83445
Katarina Tuttle and Maldon Meehan <katarina+maldon@>
2016-07-18 16:42:20 -0700

    add working update for list

--- app.rb
+++ app.rb
@@ -11,6 +11,40 @@ get('/') do
   erb(:index)
 end
 
+get("/lists/new") do
+  erb(:lists_form)
+end
+
+post("/lists") do
+  name = params.fetch("list_name")
+  @list = List.new({:name => name, :id => nil})
+  @list.save()
+  erb(:success)
+end
+
+get("/lists") do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+get('/lists/:id/edit') do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("new_list_name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  @lists = List.all()
+  erb(:lists)
+end
+
 post("/tasks") do
   description = params.fetch("description")
   task = Task.new({:description => description, :done => false})
