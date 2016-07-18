3aad9527ab45e2986a5264beb3b1b219be4ff3d9
Katarina Tuttle and Maldon Meehan <katarina+maldon@>
2016-07-18 15:12:45 -0700

    Add update method

--- app.rb
+++ app.rb
@@ -1,50 +1,32 @@
 require('sinatra')
 require('sinatra/reloader')
 require("sinatra/activerecord")
-
 also_reload('lib/**/*.rb')
-
 require('./lib/task')
 require('./lib/list')
 require('pg')
 
-
 get('/') do
+  @tasks = Task.all()
   erb(:index)
 end
 
-get("/lists/new") do
-  erb(:lists_form)
-end
-
-post("/lists") do
-  name = params.fetch("list_name")
-  @list = List.new({:name => name, :id => nil})
-  @list.save()
-  erb(:list_success)
-end
-
-get("/lists") do
-  @lists = List.all()
-  erb(:lists)
-end
-
-get("/lists/:id") do
-  @list = List.find(params.fetch("id").to_i())
-  erb(:list)
-end
-
-get('/tasks') do
-  @tasks = Task.all()
-  @list = List.find(params.fetch('id').to_i())
-  erb(:tasks)
-end ##
-
 post("/tasks") do
   description = params.fetch("description")
-  list_id = params.fetch("list_id").to_i()
-  @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
-  erb(:list_success)
+  task = Task.new({:description => description, :done => false})
+  task.save()
+  erb(:success)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
 end
